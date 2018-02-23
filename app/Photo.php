4e2e65a4bd798deed114ae7fc43962b7789d2970
ngoctam <nguyenngoctamvn@gmail.com>
2018-02-23 21:38:57 +0700

    user section working 100 percent without security

--- app/Photo.php
+++ app/Photo.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+	protected $uploads = 'http://localhost/laravel-courses/application/public/images/';
+
     protected $fillable = [
         'file'
     ];
+
+    public function getFileAttribute($photo){
+    	return $this->uploads . $photo;
+    }
 }
